2aaeb00aa7cdba91c3ae7ac88b39ae08c13f571e
Ihor Diachenko <ih.diachenko@gmail.com>
2024-01-09 16:53:02 +0200

    Updated meta descriptions

--- src/const/metadata.ts
+++ src/const/metadata.ts
@@ -14,12 +14,15 @@ const appIcon = {
 
 const manifest = new MiniAppManifest({
   appId: PackageJson.name,
-  name: 'Rarimo Proof of Humanity Verify Action',
+  name: 'Proof of Humanity',
   shortName: 'rarimo-poh-action',
   appType: 'action',
   developer: PackageJson.author,
-  description: "Repel the bots from your discord server using Rarimo's Proof of Humanity.",
-  shortDescription: "Repel the bots from your discord server using Rarimo's Proof of Humanity.",
+  descriqption: `
+    Rarimo’s Proof-of-Humanity plug-in ensures that only verified humans are present either within a server or channel. It keeps the community server spam and scam free which saves mods’ time, improves the quality of conversation, and helps protect airdrops.
+    The solution is uniquely robust, yet does not require users to reveal any personal details. Users can select between five identity providers to verify their humanity, and will receive a verifed human credential that they can use across Web3 for perks and exclusive access.
+  `,
+  shortDescription: 'Keep your server bot free using Rarimo’s Proof-of-Humanity plug-in',
   platforms: ['discord'],
   version: { name: PackageJson.version },
   website: config.pohAppUrl,
