116a495333e15829cedcc5782ea5792b500617e7
Ihor Diachenko <ih.diachenko@gmail.com>
2024-01-09 17:32:34 +0200

    Fixed formatting

--- src/const/metadata.ts
+++ src/const/metadata.ts
@@ -18,10 +18,10 @@ const manifest = new MiniAppManifest({
   shortName: 'rarimo-poh-action',
   appType: 'action',
   developer: PackageJson.author,
-  descriqption: `
-    Rarimo’s Proof-of-Humanity plug-in ensures that only verified humans are present either within a server or channel. It keeps the community server spam and scam free which saves mods’ time, improves the quality of conversation, and helps protect airdrops.
-    The solution is uniquely robust, yet does not require users to reveal any personal details. Users can select between five identity providers to verify their humanity, and will receive a verifed human credential that they can use across Web3 for perks and exclusive access.
-  `,
+  descriqption:
+    'Rarimo’s Proof-of-Humanity plug-in ensures that only verified humans are present either within a server or channel. \
+    It keeps the community server spam and scam free which saves mods’ time, improves the quality of conversation, and helps protect airdrops. \n \
+    The solution is uniquely robust, yet does not require users to reveal any personal details. Users can select between five identity providers to verify their humanity, and will receive a verifed human credential that they can use across Web3 for perks and exclusive access.',
   shortDescription: 'Keep your server bot free using Rarimo’s Proof-of-Humanity plug-in',
   platforms: ['discord'],
   version: { name: PackageJson.version },
