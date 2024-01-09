2434ac8476ff765df81522a2b6284cb9073e7619
Semen Loktionov <47495186+napalmpapalam@users.noreply.github.com>
2024-01-09 17:51:41 +0200

    Update metadata.ts

--- src/const/metadata.ts
+++ src/const/metadata.ts
@@ -18,7 +18,7 @@ const manifest = new MiniAppManifest({
   shortName: 'rarimo-poh-action',
   appType: 'action',
   developer: PackageJson.author,
-  descriqption:
+  description:
     'Rarimo’s Proof-of-Humanity plug-in ensures that only verified humans are present either within a server or channel. \
     It keeps the community server spam and scam free which saves mods’ time, improves the quality of conversation, and helps protect airdrops. \n \
     The solution is uniquely robust, yet does not require users to reveal any personal details. Users can select between five identity providers to verify their humanity, and will receive a verifed human credential that they can use across Web3 for perks and exclusive access.',
